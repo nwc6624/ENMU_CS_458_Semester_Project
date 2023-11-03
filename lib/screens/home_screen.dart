d91fc1ae505e258e490f6e2a7b5d8224cc594b57
Jake Gill <gilljacob1214@gmail.com>
2023-11-03 12:06:35 -0600

    Removed Expanded Widget.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,11 +8,6 @@ import 'safe_ride_screen.dart';
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
-  // void cardOnTap() {
-  //   Navigator.of(context)
-  //       .push(MaterialPageRoute(builder: (context) => const SafeRide()));
-  // }
-
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -21,39 +16,36 @@ class MyHomePage extends StatelessWidget {
         FocusScope.of(context).unfocus();
       },
       child: Scaffold(
+        resizeToAvoidBottomInset: false,
         body: Padding(
-          padding: const EdgeInsets.symmetric(
-            vertical: 25,
-            horizontal: 10,
-          ),
+          padding: const EdgeInsets.all(16.0),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              const Padding(
-                padding: EdgeInsets.all(16),
-                child: SearchTextField(),
-              ),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    GestureDetector(
-                      child: const SafeRideCard(
-                        title: 'SafeRide',
-                        date: 'Thursday-Sunday',
-                        time: '8am-12am',
-                      ),
-                      onTap: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => const SafeRide()));
-                      },
+              const SearchTextField(),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  GestureDetector(
+                    child: const SafeRideCard(
+                      title: 'SafeRide',
+                      date: 'Thursday-Sunday',
+                      time: '8am-12am',
                     ),
-                    const SizedBox(height: 16),
-                    const EmergencyCard(
-                      title: 'Emergency',
-                      icon: Icons.local_police,
-                    ),
-                  ],
-                ),
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => const SafeRide()));
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  const EmergencyCard(
+                    title: 'Emergency',
+                    icon: Icons.local_police,
+                  ),
+                ],
               ),
             ],
           ),
