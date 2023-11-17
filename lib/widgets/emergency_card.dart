9e1ca200063c4915695af3e8b8b5faf8d289efb2
Jake Gill <gilljacob1214@gmail.com>
2023-11-17 12:27:52 -0700

    Refactored showDialog to be continued outside of build.

--- lib/widgets/emergency_card.dart
+++ lib/widgets/emergency_card.dart
@@ -21,46 +21,52 @@ class EmergencyCard extends StatelessWidget {
     }
   }
 
+  Future<void> _showCampusPoliceAlert(BuildContext context) async {
+    showDialog<String>(
+      context: context,
+      builder: (BuildContext dialogContext) {
+        return AlertDialog(
+          title: const Text('Call Campus Police'),
+          content: const Text('Are you sure you want to call?'),
+          actionsAlignment: MainAxisAlignment.spaceBetween,
+          actions: <Widget>[
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor:
+                    Theme.of(context).colorScheme.secondaryContainer,
+              ),
+              child: const Text('Call'),
+              onPressed: () {
+                _makePhoneCall('5755622392'); // Call ENMU campus police
+              },
+            ),
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(dialogContext).pop(); // Dismiss alert dialog
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
         title: Text(
           title,
-          style: const TextStyle(fontSize: 30),
+          style: const TextStyle(
+            fontSize: 30,
+          ),
         ),
         trailing: IconButton(
           highlightColor: Colors.lightBlue,
           icon: icon,
           color: Colors.red,
-          onPressed: () => showDialog<String>(
-            context: context,
-            builder: (BuildContext dialogContext) {
-              return AlertDialog(
-                title: const Text('Call Campus Police'),
-                content: const Text('Are you sure you want to call?'),
-                actionsAlignment: MainAxisAlignment.spaceBetween,
-                actions: <Widget>[
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor:
-                          Theme.of(context).colorScheme.secondaryContainer,
-                    ),
-                    child: const Text('Call'),
-                    onPressed: () {
-                      _makePhoneCall('5755622392'); // Call ENMU campus police
-                    },
-                  ),
-                  TextButton(
-                    child: const Text('Cancel'),
-                    onPressed: () {
-                      Navigator.of(dialogContext).pop(); // Dismiss alert dialog
-                    },
-                  ),
-                ],
-              );
-            },
-          ),
+          onPressed: () => _showCampusPoliceAlert(context),
           tooltip: "Call Campus Police",
           iconSize: 35,
         ),
