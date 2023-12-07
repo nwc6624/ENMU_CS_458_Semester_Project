ccfdf95935e38e5dd56f91d711fe689d5b9d98c3
Noah <nwc6624@gmail.com>
2023-12-07 09:40:08 -0700

    Update campus_map_screen.dart
    reload for maps work

--- lib/screens/campus_map_screen.dart
+++ lib/screens/campus_map_screen.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:url_launcher/url_launcher_string.dart';
 
-import 'show_map_image.dart';
+import 'show_map_image.dart'; // Replace with your actual import
 
 class CampusMapScreen extends StatefulWidget {
   const CampusMapScreen({super.key});
@@ -13,11 +12,29 @@ class CampusMapScreen extends StatefulWidget {
   State<CampusMapScreen> createState() => _CampusMapScreenState();
 }
 
-class _CampusMapScreenState extends State<CampusMapScreen> {
+class _CampusMapScreenState extends State<CampusMapScreen> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  bool _isLoading = false;
+
   @override
   void initState() {
     super.initState();
+    _tabController = TabController(length: 4, vsync: this);
+    _tabController.addListener(_handleTabSelection);
     _checkInternetConnectivity();
+  }
+
+  @override
+  void dispose() {
+    _tabController.removeListener(_handleTabSelection);
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  void _handleTabSelection() {
+    if (_tabController.index == 0) {
+      _reloadMap();
+    }
   }
 
   Future<void> _checkInternetConnectivity() async {
@@ -44,132 +61,77 @@ class _CampusMapScreenState extends State<CampusMapScreen> {
   }
 
   Future<bool> _loadMap() async {
-    // Logic to check if the map loads
-    // For simplicity, just return true after a delay
+    // Simulated map loading logic
     await Future.delayed(const Duration(seconds: 2));
-    return true; // Replace with actual map loading logic
+    return true;
   }
 
-  Future<void> _launchMapsUrl() async {
-    const double latitude = 34.186192;
-    const double longitude = -103.334398;
-    // Google Maps URL
-    const String googleMapsUrl =
-        "https://www.google.com/maps/search/?api=1&query=$latitude,$longitude";
-    // Intent for opening the Google Maps app directly
-    const String googleMapsAppUrl =
-        "google.navigation:q=$latitude,$longitude&mode=d";
-
-    if (await canLaunchUrlString(googleMapsAppUrl)) {
-      // If Google Maps app is installed, launch it directly
-      await launchUrlString(googleMapsAppUrl);
-    } else if (await canLaunchUrlString(googleMapsUrl)) {
-      // Otherwise, open the Google Maps website
-      await launchUrlString(googleMapsUrl);
-    } else {
-      throw 'Could not launch Google Maps';
+  Future<void> _reloadMap() async {
+    if (!_isLoading) {
+      setState(() {
+        _isLoading = true;
+      });
+      await _loadMap();
+      setState(() {
+        _isLoading = false;
+      });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 4,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('ENMU Campus Map'),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.refresh),
-              onPressed: () {
-                // Refresh the state to attempt reloading the map
-                setState(() => _loadMap());
-              },
-              tooltip: 'Reload Map',
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ENMU Campus Map'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.refresh),
+            onPressed: _reloadMap,
+            tooltip: 'Reload Map',
+          ),
+        ],
+        bottom: TabBar(
+          controller: _tabController,
+          isScrollable: true,  // Make the tab bar scrollable
+          labelColor: Colors.white,
+          unselectedLabelColor: Colors.white.withOpacity(0.5),
+          tabs: const [
+            Tab(text: 'Map'),
+            Tab(text: 'Full Campus PDF'),
+            Tab(text: 'NW HWY 70 PDF'),
+            Tab(text: 'SW HWY 70 PDF'),
           ],
-          bottom: TabBar(
-            labelColor: Theme.of(context).colorScheme.onPrimary,
-            indicatorColor: Theme.of(context).colorScheme.onPrimary,
-            unselectedLabelColor: Theme.of(context).colorScheme.onPrimary,
-            isScrollable: true,
-            tabs: const [
-              Tab(text: 'Map'),
-              Tab(text: 'Full Campus PDF'),
-              Tab(text: 'NW HWY 70 PDF'),
-              Tab(text: 'SW HWY 70 PDF'),
-            ],
-          ),
         ),
-        body: SafeArea(
-          child: TabBarView(
-            children: [
-              FutureBuilder<bool>(
-                future: _loadMap(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else if (snapshot.hasError || !snapshot.data!) {
-                    // Map failed to load
-                    return Center(
-                      child: IconButton(
-                        icon: const Icon(Icons.refresh),
-                        onPressed: () {
-                          // Refresh the state to attempt reloading the map
-                          setState(() => _loadMap());
-                        },
-                        tooltip: 'Reload Map',
-                      ),
-                    );
-                  }
-                  // Map loaded successfully
-                  return buildMap(context);
-                },
-              ),
-              const MapImage(pdfPath: 'assets/pdfs/campus-map-whole.pdf'),
-              const MapImage(
-                  pdfPath: 'assets/pdfs/campus-map-northwest-us70.pdf'),
-              const MapImage(
-                  pdfPath: 'assets/pdfs/campus-map-southeast-of-us70.pdf'),
-            ],
-          ),
+      ),
+      body: SafeArea(
+        child: TabBarView(
+          controller: _tabController,
+          children: [
+            _mapView(),
+            const MapImage(pdfPath: 'assets/pdfs/campus-map-whole.pdf'),
+            const MapImage(pdfPath: 'assets/pdfs/campus-map-northwest-us70.pdf'),
+            const MapImage(pdfPath: 'assets/pdfs/campus-map-southeast-of-us70.pdf'),
+          ],
         ),
       ),
     );
   }
 
-  Widget buildMap(BuildContext context) {
-    return Stack(
+  Widget _mapView() {
+    return _isLoading
+        ? Center(child: CircularProgressIndicator())
+        : FlutterMap(
+      options: const MapOptions(
+        initialCenter: LatLng(34.186192, -103.334398),
+        initialZoom: 13.0,
+      ),
       children: [
-        FlutterMap(
-          options: const MapOptions(
-            initialCenter: LatLng(34.186192, -103.334398), // ENMU coordinates
-            initialZoom: 13.0,
-          ),
-          children: [
-            TileLayer(
-              urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-              subdomains: const ['a', 'b', 'c'],
-            ),
-          ],
-        ),
-        Align(
-          alignment: Alignment.bottomRight,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: FloatingActionButton(
-              foregroundColor:
-                  Theme.of(context).colorScheme.onSecondaryContainer,
-              backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
-              onPressed: () {
-                //TODO: Open device navigation app
-                // _launchMapsUrl();
-              },
-              child: const Icon(Icons.navigation_outlined),
-            ),
-          ),
+        TileLayer(
+          urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+          subdomains: const ['a', 'b', 'c'],
         ),
       ],
     );
   }
 }
+
